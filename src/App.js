ba951bef31fa634d5a84ead1f5795aee5ad4cfd3
Darko Mijatovic <darkomijatovic2022@gmail.com>
2024-03-01 13:46:44 +0100

    dark mode added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
 // https://api.github.com/users/:username
 function App() {
-  const [name, setName] = useState("");
+  const [name, setName] = useState("darko96");
   const [user, setUser] = useState("");
+  const [theme, setTheme] = useState("light");
+
+  console.log(theme);
+
+  function handleTheme() {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  }
 
   function handleAddUser(name) {
     setName(name);
@@ -14,7 +21,7 @@ function App() {
     function () {
       if (name.length > 1) {
         async function fetchUser() {
-          const response = await fetch(`https://api.github.com/users/:${name}`);
+          const response = await fetch(`https://api.github.com/users/${name}`);
           const data = await response.json();
           setUser(data);
         }
@@ -25,11 +32,11 @@ function App() {
   );
 
   return (
-    <main>
+    <main id={theme}>
       <div className="container">
-        <Header></Header>
+        <Header theme={theme} onHandleTheme={handleTheme}></Header>
         <Search name={name} onAddName={handleAddUser} />
-        <User></User>
+        <User user={user}></User>
       </div>
     </main>
   );
@@ -37,43 +44,49 @@ function App() {
 
 export default App;
 
-function Header() {
+function Header({ theme, onHandleTheme }) {
   return (
     <header className="header">
       <h1 className="logo">devfinder</h1>
-      <Theme />
+      <Theme theme={theme} onHandleTheme={onHandleTheme} />
     </header>
   );
 }
 
-function Theme() {
+function Theme({ theme, onHandleTheme }) {
   return (
     <div className="theme">
-      <span className="theme-text">Light</span> {/* Dark */}
-      <button className="theme-button">
-        {/* moon */}
-        <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="M19.513 11.397a.701.701 0 00-.588.128 7.496 7.496 0 01-2.276 1.336 7.101 7.101 0 01-2.583.462 7.505 7.505 0 01-5.32-2.209 7.568 7.568 0 01-2.199-5.342c0-.873.154-1.72.41-2.49a6.904 6.904 0 011.227-2.21.657.657 0 00-.102-.924.701.701 0 00-.589-.128C5.32.61 3.427 1.92 2.072 3.666A10.158 10.158 0 000 9.83c0 2.8 1.125 5.342 2.967 7.19a10.025 10.025 0 007.16 2.98c2.353 0 4.527-.822 6.266-2.183a10.13 10.13 0 003.58-5.624.623.623 0 00-.46-.796z"
-            fill="#697C9A"
-            fillRule="nonzero"
-          />
-        </svg>
-        {/* sun */}
-        {/* <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg">
-          <g fill="#FFF" fillRule="nonzero">
-            <path d="M13.545 6.455c-.9-.9-2.17-1.481-3.545-1.481a4.934 4.934 0 00-3.545 1.481c-.9.9-1.481 2.17-1.481 3.545 0 1.376.582 2.646 1.481 3.545.9.9 2.17 1.481 3.545 1.481a4.934 4.934 0 003.545-1.481c.9-.9 1.481-2.17 1.481-3.545a4.934 4.934 0 00-1.481-3.545zM10 3.413a.7.7 0 00.688-.688V.688A.7.7 0 0010 0a.7.7 0 00-.688.688v2.037a.7.7 0 00.688.688zM15.635 5.344l1.455-1.455a.67.67 0 000-.952.67.67 0 00-.952 0l-1.455 1.455a.67.67 0 000 .952c.238.264.66.264.952 0zM19.312 9.312h-2.037a.7.7 0 00-.688.688.7.7 0 00.688.688h2.037A.7.7 0 0020 10a.7.7 0 00-.688-.688zM15.608 14.656a.67.67 0 00-.952 0 .67.67 0 000 .952l1.455 1.455a.67.67 0 00.952 0 .67.67 0 000-.952l-1.455-1.455zM10 16.587a.7.7 0 00-.688.688v2.037A.7.7 0 0010 20a.7.7 0 00.688-.688v-2.037a.7.7 0 00-.688-.688zM4.365 14.656L2.91 16.111a.67.67 0 000 .952.67.67 0 00.952 0l1.455-1.455a.67.67 0 000-.952c-.238-.264-.66-.264-.952 0zM3.413 10a.7.7 0 00-.688-.688H.688A.7.7 0 000 10a.7.7 0 00.688.688h2.037A.7.7 0 003.413 10zM4.365 5.344a.67.67 0 00.952 0 .67.67 0 000-.952L3.862 2.937a.67.67 0 00-.952 0 .67.67 0 000 .952l1.455 1.455z" />
-          </g>
-        </svg> */}
+      {theme === "light" && <span className="theme-text">Dark</span>}
+      {theme === "dark" && <span className="theme-text">Light</span>}
+
+      <button className="theme-button" onClick={onHandleTheme}>
+        {theme === "light" && (
+          <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M19.513 11.397a.701.701 0 00-.588.128 7.496 7.496 0 01-2.276 1.336 7.101 7.101 0 01-2.583.462 7.505 7.505 0 01-5.32-2.209 7.568 7.568 0 01-2.199-5.342c0-.873.154-1.72.41-2.49a6.904 6.904 0 011.227-2.21.657.657 0 00-.102-.924.701.701 0 00-.589-.128C5.32.61 3.427 1.92 2.072 3.666A10.158 10.158 0 000 9.83c0 2.8 1.125 5.342 2.967 7.19a10.025 10.025 0 007.16 2.98c2.353 0 4.527-.822 6.266-2.183a10.13 10.13 0 003.58-5.624.623.623 0 00-.46-.796z"
+              fill="#697C9A"
+              fillRule="nonzero"
+            />
+          </svg>
+        )}
+        {theme === "dark" && (
+          <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg">
+            <g fill="#fff" fillRule="nonzero">
+              <path d="M13.545 6.455c-.9-.9-2.17-1.481-3.545-1.481a4.934 4.934 0 00-3.545 1.481c-.9.9-1.481 2.17-1.481 3.545 0 1.376.582 2.646 1.481 3.545.9.9 2.17 1.481 3.545 1.481a4.934 4.934 0 003.545-1.481c.9-.9 1.481-2.17 1.481-3.545a4.934 4.934 0 00-1.481-3.545zM10 3.413a.7.7 0 00.688-.688V.688A.7.7 0 0010 0a.7.7 0 00-.688.688v2.037a.7.7 0 00.688.688zM15.635 5.344l1.455-1.455a.67.67 0 000-.952.67.67 0 00-.952 0l-1.455 1.455a.67.67 0 000 .952c.238.264.66.264.952 0zM19.312 9.312h-2.037a.7.7 0 00-.688.688.7.7 0 00.688.688h2.037A.7.7 0 0020 10a.7.7 0 00-.688-.688zM15.608 14.656a.67.67 0 00-.952 0 .67.67 0 000 .952l1.455 1.455a.67.67 0 00.952 0 .67.67 0 000-.952l-1.455-1.455zM10 16.587a.7.7 0 00-.688.688v2.037A.7.7 0 0010 20a.7.7 0 00.688-.688v-2.037a.7.7 0 00-.688-.688zM4.365 14.656L2.91 16.111a.67.67 0 000 .952.67.67 0 00.952 0l1.455-1.455a.67.67 0 000-.952c-.238-.264-.66-.264-.952 0zM3.413 10a.7.7 0 00-.688-.688H.688A.7.7 0 000 10a.7.7 0 00.688.688h2.037A.7.7 0 003.413 10zM4.365 5.344a.67.67 0 00.952 0 .67.67 0 000-.952L3.862 2.937a.67.67 0 00-.952 0 .67.67 0 000 .952l1.455 1.455z" />
+            </g>
+          </svg>
+        )}
       </button>
     </div>
   );
 }
 
-function Search({ name, onAddName }) {
+function Search({ onAddName }) {
+  const nameRef = useRef("");
+
   return (
     <div className="search">
-      <div>
+      <div className="input-box">
         <span className="search-icon">
           <svg height="24" width="25" xmlns="http://www.w3.org/2000/svg">
             <path
@@ -83,64 +96,96 @@ function Search({ name, onAddName }) {
           </svg>
         </span>
         <input
-          value={name}
-          onChange={(event) => onAddName(event.target.value)}
+          ref={nameRef}
+          onChange={(event) => (nameRef.current = event.target.value)}
           type="text"
           className="search-input"
           placeholder="GitHub username..."
         />
       </div>
-      <button className="search-button">Search</button>
+      <button
+        onClick={() => {
+          onAddName(nameRef.current);
+        }}
+        className="search-button"
+      >
+        Search
+      </button>
     </div>
   );
 }
 
-function User() {
+function User({ user }) {
   return (
     <div className="user">
-      <Bio></Bio>
-      <GitHubNumbers></GitHubNumbers>
-      <Links></Links>
+      <Bio user={user} />
+      <GitHubNumbers user={user} />
+      <Links user={user} />
     </div>
   );
 }
 
-function Bio() {
+function Bio({ user }) {
+  let dateText = "2021-12-01T08:49:03Z";
+  let date = new Date(dateText);
+
+  let year = date.getFullYear();
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "Maj",
+    "Jun",
+    "Jul",
+    "Avg",
+    "Sep",
+    "Okt",
+    "Nov",
+    "Dec",
+  ];
+  let month = months[date.getMonth()];
+  let day = date.getDate();
+
   return (
     <div className="bio">
-      <div className="user-image">{/* <img></img> */}</div>
+      <div>
+        <img className="user-image" src={user.avatar_url} alt={user.name}></img>
+      </div>
       <div className="user-details">
-        <h2 className="user-name">Name</h2>
-        <span className="username">username</span>
-        <p className="user-bio">This profile has no bio</p>
+        <h2 className="user-name">{user.name}</h2>
+        <span className="username">{user.login}</span>
+        <p className="user-bio">
+          {user.bio ? user.bio : `This profile has no bio`}
+        </p>
       </div>
       <div className="user-joined-date">
-        <p className="user-joined-date-text">Joined 25 Jan 2011</p>
+        <p className="user-joined-date-text">{`Joined ${day} ${month} ${year}`}</p>
       </div>
     </div>
   );
 }
 
-function GitHubNumbers() {
+function GitHubNumbers({ user }) {
   return (
     <div className="github-numbers">
       <div>
         <span className="github-text">Repos</span>
-        <span className="github-number">8</span>
+        <span className="github-number">{user.public_repos}</span>
       </div>
       <div>
         <span className="github-text">Followers</span>
-        <span className="github-number">3938</span>
+        <span className="github-number">{user.followers}</span>
       </div>
       <div>
         <span className="github-text">Following</span>
-        <span className="github-number">9</span>
+        <span className="github-number">{user.following}</span>
       </div>
     </div>
   );
 }
 
-function Links() {
+function Links({ user }) {
   return (
     <div className="links">
       <div className="link">
@@ -152,7 +197,7 @@ function Links() {
             />
           </svg>
         </span>
-        <span className="link-text">Belgrad</span>
+        <span className="link-text">{user.location}</span>
       </div>
       <div className="link">
         <span className="link-img">
@@ -163,7 +208,9 @@ function Links() {
             />
           </svg>
         </span>
-        <span className="link-text">www.google.com</span>
+        <span className="link-text disabled">
+          {user.twitter_username ? user.twitter_username : `Not Available`}
+        </span>
       </div>
       <div className="link">
         <span className="link-img">
@@ -174,7 +221,9 @@ function Links() {
             </g>
           </svg>
         </span>
-        <span className="link-text">Not Available</span>
+        <a href={user.blog} className="link-text">
+          {user.blog}
+        </a>
       </div>
       <div className="link">
         <span className="link-img">
@@ -184,7 +233,7 @@ function Links() {
             </g>
           </svg>
         </span>
-        <span className="link-text">Google</span>
+        <span className="link-text">{user.company}</span>
       </div>
     </div>
   );
